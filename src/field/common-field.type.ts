d0ce94b304cadbcf3756aef5b99e8919c06b2049
TheNightmareX <TheNightmareX@foxmail.com>
2021-08-19 20:54:54 +0800

    chore: fix comment typo

--- src/field/common-field.type.ts
+++ src/field/common-field.type.ts
@@ -5,7 +5,7 @@ import { RelationField } from "./relation-field.type";
 export type CommonField<Entity extends BaseEntity> = Exclude<
   EntityField<Entity>,
   /**
-   * Relation fields is not possible to be `string` unless `Entity` is
+   * Relation fields are not possible to be `string` unless `Entity` is
    * `AnyEntity`, if so, `Exclude<string, string>` will be `never` and cause
    * type errors. So it is required to make sure this type return `string` in
    * this case.

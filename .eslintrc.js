c8112aaff549b4d2cd9dfb7e3841cc5a31ef3d8f
Joshua Chen <sidachen2003@gmail.com>
2021-08-24 09:57:55 +0800

    chore: add ESLint & GitHub CI (#3)
    * Add eslint
    
    * Fix typo
    
    * Update rule
    
    * Properly install husky
    
    * Bring back the good old NPM
    
    * Update husky

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    commonjs: true,
+    jest: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {argsIgnorePattern: '^_', ignoreRestSiblings: true},
+    ],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+  }
+};

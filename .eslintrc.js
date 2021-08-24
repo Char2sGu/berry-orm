8b845de0164342f99f69c4d0a20c49f9bae61beb
Josh-Cena <sidachen2003@gmail.com>
2021-08-24 08:27:15 +0800

    Update rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,12 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
-  ]
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {argsIgnorePattern: '^_', ignoreRestSiblings: true},
+    ],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+  }
 };
--- src/entity/identity-map.class.ts
+++ src/entity/identity-map.class.ts
@@ -2,6 +2,8 @@ import { PrimaryField } from "../field/primary-field.type";
 import { BaseEntity } from "./base-entity.class";
 
 export class IdentityMap<Entity extends BaseEntity> extends Map<
+  // Should be fixed by setting "argsIgnorePattern" but it's not?
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   Entity[Entity extends BaseEntity<infer _, infer Primary>
     ? Primary extends PrimaryField<Entity>
       ? Primary

2f67d08ee790a56b80ce2407cc0ae716b4204ccd
Josh-Cena <sidachen2003@gmail.com>
2021-08-24 08:18:46 +0800

    Fix typo

--- src/field/entity-field.type.ts
+++ src/field/entity-field.type.ts
@@ -2,6 +2,6 @@ import { BaseEntity } from "../entity/base-entity.class";
 import { ExcludeKeys } from "../utils/exclude-keys.type";
 
 export type EntityField<Entity extends BaseEntity> = string &
-  // We are excluding Functions here, so it's fine to be generci
+  // We are excluding Functions here, so it's fine to be generic
   // eslint-disable-next-line @typescript-eslint/ban-types
   ExcludeKeys<Entity, Function>;

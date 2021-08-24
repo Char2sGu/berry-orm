6b4a08b61ff249c24f69b589f8c7a78d61650ea8
TheNightmareX <TheNightmareX@foxmail.com>
2021-08-24 14:09:02 +0800

    style: format config files too

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,18 +6,18 @@ module.exports = {
     jest: true,
     node: true,
   },
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
   ],
   rules: {
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      {argsIgnorePattern: '^_', ignoreRestSiblings: true},
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { argsIgnorePattern: "^_", ignoreRestSiblings: true },
     ],
-    '@typescript-eslint/no-non-null-assertion': 'off',
-  }
+    "@typescript-eslint/no-non-null-assertion": "off",
+  },
 };

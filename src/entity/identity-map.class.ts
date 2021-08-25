4f02c5be6f5d946d3b615ef0841f7df62d8a35d9
TheNightmareX <TheNightmareX@foxmail.com>
2021-08-25 09:35:27 +0800

    refactor: avoid useless `infer` statements

--- src/entity/identity-map.class.ts
+++ src/entity/identity-map.class.ts
@@ -2,9 +2,8 @@ import { PrimaryField } from "../field/primary-field.type";
 import { BaseEntity } from "./base-entity.class";
 
 export class IdentityMap<Entity extends BaseEntity> extends Map<
-  // Should be fixed by setting "argsIgnorePattern" but it's not?
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  Entity[Entity extends BaseEntity<infer _, infer Primary>
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  Entity[Entity extends BaseEntity<any, infer Primary>
     ? Primary extends PrimaryField<Entity>
       ? Primary
       : never

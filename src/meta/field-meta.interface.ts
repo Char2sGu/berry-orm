d3f242bafd444d3123338e57abaf2931b67497f4
TheNightmareX <TheNightmareX@foxmail.com>
2021-08-27 22:59:14 +0800

    refactor: add a second generic parameter for `FieldMeta`

--- src/meta/field-meta.interface.ts
+++ src/meta/field-meta.interface.ts
@@ -2,8 +2,11 @@ import { AnyEntity } from "..";
 import { EntityType } from "../entity/entity-type.type";
 import { EntityField } from "../field/entity-field.type";
 
-export interface FieldMeta<Entity extends AnyEntity = AnyEntity> {
-  name: EntityField<Entity>;
+export interface FieldMeta<
+  Entity extends AnyEntity = AnyEntity,
+  Field extends EntityField<Entity> = EntityField<Entity>,
+> {
+  name: Field;
   relation: {
     target: () => EntityType<AnyEntity>;
     inverse: string;

e7fa356392395c453a6d104060a61b9324d8999b
TheNightmareX <TheNightmareX@foxmail.com>
2021-12-19 01:17:54 +0800

    refactor: perform all inspections in `BerryOrm`

--- src/berry-orm.class.ts
+++ src/berry-orm.class.ts
@@ -1,54 +1,90 @@
 import { AnyEntity } from "./entity/any-entity.type";
 import { EntityType } from "./entity/entity-type.interface";
+import { EntityField } from "./field/field-names/entity-field.type";
 import { EntityManager } from "./managers/entity-manager.class";
 import { RelationManager } from "./managers/relation-manager.class";
 import { EntityMetaError } from "./meta/entity-meta.error";
 import { META } from "./symbols";
 
 export class BerryOrm {
-  readonly registry: EntityRegistry;
+  readonly registry: Set<EntityType>;
   readonly em: EntityManager;
   readonly rm: RelationManager;
 
   constructor(options: BerryOrmOptions) {
-    this.registry = this.inspect(new Set(options.entities));
+    this.registry = new Set(options.entities);
+    this.inspect();
     this.em = new EntityManager(this);
     this.rm = new RelationManager(this);
   }
 
-  /**
-   * Check whether the entity registry can work correctly.
-   */
-  private inspect<Registry extends EntityRegistry>(
-    registry: Registry,
-  ): Registry {
+  private inspect() {
     const names = new Set<string>();
 
-    registry.forEach((type) => {
+    this.registry.forEach((type) => {
       if (names.has(type.name)) throw new Error("Entity names must be unique");
       names.add(type.name);
-
-      if (!type.prototype[META])
-        throw new EntityMetaError({ type, message: "Must be decorated" });
+      this.inspectEntity(type);
     });
+  }
 
-    registry.forEach((type) => {
-      type.prototype[META]!.inspect();
-      Object.values(type.prototype[META]!.fields).forEach((meta) => {
-        if (!meta.relation) return;
-        if (!registry.has(meta.relation.target()))
-          throw new EntityMetaError({
-            type,
-            field: meta.name,
-            message: "The relation entity must be also registered",
-          });
+  private inspectEntity<Entity extends AnyEntity>(type: EntityType<Entity>) {
+    const meta = type.prototype[META];
+
+    if (!meta)
+      throw new EntityMetaError({ type, message: "Must be decorated" });
+
+    if (!meta.primary)
+      throw new EntityMetaError({
+        type,
+        message: "Must have a primary field registered",
       });
-    });
-    return registry;
+
+    for (const field in meta.fields) {
+      this.inspectEntityField(type, field as EntityField<Entity>);
+    }
+  }
+
+  private inspectEntityField<Entity extends AnyEntity>(
+    type: EntityType<Entity>,
+    field: EntityField<Entity>,
+  ) {
+    const meta = type.prototype[META]!.fields[field];
+    if (meta.relation) {
+      if (!this.registry.has(meta.relation.target()))
+        throw new EntityMetaError({
+          type,
+          field: meta.name,
+          message: "The relation entity must be also registered",
+        });
+
+      const metaInverse =
+        meta.relation.target().prototype[META]!.fields[meta.relation.inverse];
+
+      if (!metaInverse.relation)
+        throw new EntityMetaError({
+          type,
+          field,
+          message: "The inverse side must be a relation field",
+        });
+
+      if (metaInverse.relation.target() != type)
+        throw new EntityMetaError({
+          type,
+          field,
+          message: "The inverse side must point back to this entity",
+        });
+
+      if (metaInverse.relation.inverse != field)
+        throw new EntityMetaError({
+          type,
+          field,
+          message:
+            "The inverse side of the inverse side must point back to this field",
+        });
+    }
   }
 }
-
-type EntityRegistry = Set<EntityType>;
 
 interface BerryOrmOptions {
   entities: EntityType<AnyEntity>[];
--- src/meta/meta-objects/entity-meta.class.ts
+++ src/meta/meta-objects/entity-meta.class.ts
@@ -1,8 +1,6 @@
 import { EntityField, EntityPrimaryField } from "../..";
 import { AnyEntity } from "../../entity/any-entity.type";
 import { EntityType } from "../../entity/entity-type.interface";
-import { META } from "../../symbols";
-import { EntityMetaError } from "../entity-meta.error";
 import { EntityMetaField } from "./entity-meta-field.class";
 
 export class EntityMeta<
@@ -13,45 +11,4 @@ export class EntityMeta<
   fields = {} as Record<EntityField<Entity>, EntityMetaField<Entity>>;
 
   constructor(readonly type: EntityType<Entity>) {}
-
-  /**
-   *
-   * Check whether the decorators are applied correctly in this entity.
-   * @param mutual - Specify whether to check the other side of the relation.
-   */
-  inspect(): void {
-    if (!this.primary)
-      throw new EntityMetaError({
-        type: this.type,
-        message: "Must have a primary field registered",
-      });
-
-    for (const k in this.fields) {
-      const field = k as EntityField<Entity>;
-      const meta = this.fields[field];
-      if (meta.relation) {
-        const inverseMeta =
-          meta.relation.target().prototype[META]!.fields[meta.relation.inverse];
-        if (!inverseMeta.relation)
-          throw new EntityMetaError({
-            type: this.type,
-            field,
-            message: "The inverse side must be a relation field",
-          });
-        if (inverseMeta.relation.target() != this.type)
-          throw new EntityMetaError({
-            type: this.type,
-            field,
-            message: "The inverse side must point back to this entity",
-          });
-        if (inverseMeta.relation.inverse != field)
-          throw new EntityMetaError({
-            type: this.type,
-            field,
-            message:
-              "The inverse side of the inverse side must point back to this field",
-          });
-      }
-    }
-  }
 }

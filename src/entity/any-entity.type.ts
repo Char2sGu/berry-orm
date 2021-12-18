ecad02f898a9dbe93a7d1cd6d71027cc265a5248
TheNightmareX <TheNightmareX@foxmail.com>
2021-12-18 19:00:25 +0800

    style(eslint): remove the expired rule comments

--- src/entity/any-entity.type.ts
+++ src/entity/any-entity.type.ts
@@ -5,9 +5,6 @@ import { BaseEntity } from "./base-entity.class";
  * Represent an entity extending the `BaseEntity`.
  */
 export type AnyEntity<
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   Entity extends AnyEntity<Entity, Primary> = any,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   Primary extends PrimaryField<Entity> = any,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
 > = BaseEntity<Entity, Primary> & Record<string, any>;
--- src/field/entity-primary-field.type.ts
+++ src/field/entity-primary-field.type.ts
@@ -2,7 +2,6 @@ import { AnyEntity } from "../entity/any-entity.type";
 import { PrimaryField } from "./primary-field.type";
 
 export type EntityPrimaryField<Entity extends AnyEntity> =
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   Entity extends AnyEntity<any, infer Primary>
     ? Primary extends PrimaryField<Entity>
       ? Primary

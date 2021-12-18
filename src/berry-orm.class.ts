1ba000aa3e919037d57ad76b6457510c22b10f37
TheNightmareX <TheNightmareX@foxmail.com>
2021-12-19 01:04:30 +0800

    feat(orm): add duplicate entity name check

--- src/berry-orm.class.ts
+++ src/berry-orm.class.ts
@@ -22,10 +22,16 @@ export class BerryOrm {
   private inspect<Registry extends EntityRegistry>(
     registry: Registry,
   ): Registry {
+    const names = new Set<string>();
+
     registry.forEach((type) => {
+      if (names.has(type.name)) throw new Error("Entity names must be unique");
+      names.add(type.name);
+
       if (!type.prototype[META])
         throw new EntityMetaError({ type, message: "Must be decorated" });
     });
+
     registry.forEach((type) => {
       type.prototype[META]!.inspect();
       Object.values(type.prototype[META]!.fields).forEach((meta) => {

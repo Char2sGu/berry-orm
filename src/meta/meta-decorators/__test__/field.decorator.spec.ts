1e469f9a00da17a5bcd57a99f2e5e26325416c14
TheNightmareX <TheNightmareX@foxmail.com>
2021-12-24 20:32:17 +0800

    test(meta/decorators/field): update

--- src/meta/meta-decorators/__test__/field.decorator.spec.ts
+++ src/meta/meta-decorators/__test__/field.decorator.spec.ts
@@ -1,6 +1,7 @@
 import { BaseEntity } from "../../../entity/base-entity.class";
 import { EntityType } from "../../../entity/entity-type.interface";
 import { META } from "../../../symbols";
+import { EntityMetaError } from "../../entity-meta.error";
 import { EntityFieldMeta } from "../../meta-objects/entity-field-meta.class";
 import { EntityMeta } from "../../meta-objects/entity-meta.class";
 import { Field } from "../field.decorator";
@@ -12,15 +13,23 @@ describe("@Field()", () => {
     cls = class TestingEntity extends BaseEntity<TestingEntity, "id"> {
       id!: number;
     };
-    Field()(cls.prototype, "id");
   });
 
   it("should define the metadata of the entity", () => {
+    Field()(cls.prototype, "id");
     expect(cls.prototype[META]).toBeInstanceOf(EntityMeta);
   });
 
   it("should define the metadata of the field", () => {
+    Field()(cls.prototype, "id");
     const meta = cls.prototype[META]?.fields.id;
     expect(meta).toBeInstanceOf(EntityFieldMeta);
+  });
+
+  it("should throw an error when applied for multiple times", () => {
+    Field()(cls.prototype, "id");
+    expect(() => {
+      Field()(cls.prototype, "id");
+    }).toThrow(EntityMetaError);
   });
 });
